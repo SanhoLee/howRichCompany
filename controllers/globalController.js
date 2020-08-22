2b046575bd5142ca68984bff3a229be04ece44d8
SanhoLee <stlsh0516@gmail.com>
2020-08-22 21:47:24 +0900

    1.8 XML Parsing Data Cleansing.

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import fs from "fs";
 import { text } from "body-parser";
-import xml2Json from "xml-js";
+import converter from "xml-js";
+import { data } from "autoprefixer";
 
 dotenv.config();
 
@@ -22,19 +23,67 @@ export const handleHome = async (req, res) => {
   }
 };
 
-const getCorpList = () => {
-  const xml = fs.readFile("../CORPCODE.xml", "utf8", (err, data) => {
-    if (err) throw err;
-    const parsingOption = {
-      compact: true,
-      spaces: 4,
-      ignoreAttributes: true,
-      ignoreDeclaration: true,
-    };
+// const getCorpList = () => {
+//   const xml = fs.readFile("../CORPCODE.xml", "utf8", (err, data) => {
+//     if (err) throw err;
+//     const option = {
+//       compact: true,
+//       spaces: 4,
+//       ignoreAttributes: true,
+//       ignoreDeclaration: true,
+//     };
 
-    const jsonObj = xml2Json.xml2js(data, parsingOption);
-    console.log(`Total Company Count is ... :${jsonObj.result.list.length}`);
-  });
+//     const jsonObj = converter.xml2js(data, option);
+//     const corpList = jsonObj.result.list;
+//     console.log(`Total Company Count is ... :${jsonObj.result.list.length}`);
+//   });
+// };
+
+// refereced by https://github.com/nashwaan/xml-js/issues/53
+const nativeType = (value) => {
+  const nValue = Number(value);
+  if (!isNaN(nValue)) {
+    return nValue;
+  }
+  const bValue = value.toLowerCase();
+  if (bValue === "true") {
+    return true;
+  } else if (bValue === "false") {
+    return false;
+  }
+  return value;
+};
+
+// refereced by https://github.com/nashwaan/xml-js/issues/53
+const removeJsonTextAttribute = (value, parentElement) => {
+  try {
+    const keyNo = Object.keys(parentElement._parent).length;
+    const keyName = Object.keys(parentElement._parent)[keyNo - 1];
+    parentElement._parent[keyName] = nativeType(value);
+  } catch (e) {
+    console.log(`ERROR at removeJsonTextAttribute : ${e}`);
+  }
+};
+
+const getCorpList = () => {
+  const xml = fs.readFileSync("../CORPCODE.xml", "utf8", "r");
+  const option = {
+    compact: true,
+    trim: true,
+    ignoreDeclaration: true,
+    ignoreInstruction: true,
+    ignoreAttributes: true,
+    ignoreComment: true,
+    ignoreCdata: true,
+    ignoreDoctype: true,
+    textFn: removeJsonTextAttribute,
+  };
+
+  const jsonObj = converter.xml2js(xml, option);
+  const totalCorps = jsonObj.result.list;
+  console.log(totalCorps);
+  // const corpList = jsonObj.result.list;
+  // console.log(`Total Company Count is ... :${jsonObj.result.list.length}`);
 };
 
 export const getSearch = async (req, res) => {
@@ -42,12 +91,11 @@ export const getSearch = async (req, res) => {
     query: { term: searchingBy },
   } = req;
   try {
-    await getCorpList();
+    const totalCorps = await getCorpList();
+    res.render("search", { pagetitle: "Search", searchingBy, totalCorps });
   } catch (error) {
     console.log(`getCorpList has some ERROR : ${error}`);
   }
-
-  res.render("search", { pagetitle: "Search", searchingBy });
 };
 
 export const postSearch = (req, res) => {};
