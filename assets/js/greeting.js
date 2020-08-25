86af1f42d2432cdb055797a7f35fad0eb0097420
SanhoLee <stlsh0516@gmail.com>
2020-08-25 22:25:37 +0900

    1.12 set Nicknane on Cookie data.

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -1,25 +1,55 @@
-const nickName = document.getElementById("jsNickname");
-const formDiv = nickName.lastChild;
-const form = formDiv.querySelector("form");
+const loginForm = document.getElementById("jsNickname");
+const body = document.querySelector("body");
+const greeting = document.getElementById("jsGreeting");
+const searchBox = document.getElementById("jsSearchBox");
 
-const SHOWING_ON = "showing";
+const NICKNAME = "nickname";
 
-const handleSubmit = (event) => {
-  event.preventDefault;
-  const currentValue = input.value;
-  localStorage.setItem("nickname", currentValue);
+const LOGGED_IN = "loggedIn";
+const LOGGED_OUT = "loggedOut";
+
+const nickname = localStorage.getItem(NICKNAME);
+
+const paintGreeting = (text) => {
+  const greetingMsg = `Hello ${text} !!!`;
+  greeting.innerText = greetingMsg;
+  body.className = LOGGED_IN;
 };
 
+const setName = (text) => {
+  localStorage.setItem(NICKNAME, text);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = loginForm.querySelector("input");
+  const { value } = input;
+  input.value = "";
+  paintGreeting(value);
+  setName(value);
+};
+
+if (nickname === null) {
+  body.className = LOGGED_OUT;
+} else {
+  body.className = LOGGED_IN;
+}
+
 const askForName = () => {
-  form.classList.add(SHOWING_ON);
-  form.addEventListener("submit", handleSubmit);
+  loginForm.addEventListener("submit", handleSubmit);
+};
+
+const loadName = () => {
+  const currentUser = localStorage.getItem(NICKNAME);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
 };
 
 const init = () => {
-  askForName();
-  console.log("hello");
+  loadName();
 };
 
-if (form) {
-  init();
-}
+init();
