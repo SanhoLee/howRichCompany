b643e4705307cbecae96ed1762fbed3dc50d83f1
SanhoLee <stlsh0516@gmail.com>
2020-09-05 17:37:12 +0900

    1.20 sending corp code and name info for the cart page

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,14 +1,53 @@
+import { NULL } from "node-sass";
+
 // if method for get,, req.query has info.
 export const getCart = (req, res) => {
   res.render("cart", { pagetitle: "Cart" });
 };
 
+const makeArrayFormat = (_element) => {
+  if (typeof _element !== "object") {
+    // in case of having one element just like this,,,,  _element = "something"
+    return [_element];
+  } else {
+    // in case of having more than one element,,, _element = ["000000", "1123123"...]
+    return _element;
+  }
+};
+
+const makeListToObject = (corp_info) => {
+  let temp_obj = new Object();
+  temp_obj.corp_name = corp_info[0];
+  temp_obj.corp_code = corp_info[1];
+  return temp_obj;
+};
+
+const splitString = (target, delimiter) => {
+  return target.split(delimiter);
+};
+
+const makeCartData = (targetList) => {
+  // targetList is array element, looks like..."corp_name, corp_code"
+  // So it needs divide with , delimeter
+  let list_basket = [];
+  targetList.map((potato) => {
+    const split_corp_info = splitString(potato, ",");
+    const corp_obj = makeListToObject(split_corp_info);
+    list_basket.push(corp_obj);
+  });
+  return list_basket;
+};
+
 // if method for post,, req.body has info.
 export const postCart = (req, res) => {
   const {
-    body: { corp_code: checkedCorps },
+    body: { corp_data: checkedBlocks },
   } = req;
-  console.log(checkedCorps);
 
-  res.render("cart", { pagetitle: "Cart", checkedCorps });
+  let checkedCorps = NULL;
+  checkedCorps = makeArrayFormat(checkedBlocks);
+  const cartCorps = makeCartData(checkedCorps);
+  console.log(cartCorps);
+
+  res.render("cart", { pagetitle: "Cart", cartCorps });
 };
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,18 +1,14 @@
 import express from "express";
 import routes from "../routes";
-import {
-  handleHome,
-  getSearch,
-  postSearch,
-} from "../controllers/globalController";
+import { handleHome, getSearch } from "../controllers/globalController";
 import { getCart, postCart } from "../controllers/cartController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, handleHome);
 globalRouter.get(routes.search, getSearch);
-// globalRouter.post(routes.search, postSearch);
 
+globalRouter.get(routes.cart, getCart);
 globalRouter.post(routes.cart, postCart);
 
 export default globalRouter;
